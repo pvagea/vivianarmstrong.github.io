6f20cdc699c60fb0747daa09582a4e0489563380
pvagea <arm18006@byui.edu>
2020-02-08 09:24:32 -0800

    e

---
+++ lesson5/js/banner.js
@@ -0,0 +1,7 @@
+function showBanner() {
+    var theBanner = document.getElementById('hero-image');
+    
+    var theTime = new Date();
+    var theWeekday = theTime.getDay();
+    var banner = theBanner[Friday];
+}
