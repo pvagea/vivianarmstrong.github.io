37c3a83f9560430e4405a40a2d884dd508525816
pvagea <arm18006@byui.edu>
2020-02-08 10:47:27 -0800

    e

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,7 +1,5 @@
-function showBanner() {
-    var theBanner = document.getElementById('hero-image');
-    
-    var theTime = new Date();
-    var theWeekday = theTime.getDay();
-    var banner = theBanner[Friday];
+var today = new Date();
+var date = today.getDay();
+if (date == 5) {
+    document.write("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
 }
