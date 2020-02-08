e75e5d4d95259c6b9fb220efb177f610345c8be6
pvagea <arm18006@byui.edu>
2020-02-08 11:33:32 -0800

    e

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var date = today.getDay();
 if (date == 5) {
-    document.write("<center>"+"Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."+"</center>")
+    document.write("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
 }
 
 else {
