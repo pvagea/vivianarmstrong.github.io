4b9eef99824be313f736e6c1132eff214bfc3a39
pvagea <arm18006@byui.edu>
2020-02-15 11:45:11 -0800

    f

--- lesson6/js/date.js
+++ lesson6/js/date.js
@@ -1,9 +1,36 @@
 //Code for copyright and current date at the bottom of the webpage
-var now = new Date();
+let d = new Date();
+let currentYear = d.getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
 
-var Weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-var Month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-document.write(Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let month = new Array(12);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+let currMonth = month[d.getMonth()];
+let currDay = weekday[d.getDay()];
+let stringDate = currDay + ", " + d.getDate() + " " + currMonth + " " + currentYear;
+document.getElementById("todayDate").textContent = stringDate;
+
 
 
 //Code for the banner message at the top of the webpage to show up only on Friday
