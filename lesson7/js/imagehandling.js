59913d1497fe7c3d78604141dd598a4002e9e749
pvagea <arm18006@byui.edu>
2020-02-20 18:26:13 -0800

    g

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -7,21 +7,31 @@ const imgOptions = {
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
 
-if('IntersectionObserver' in window) {
+if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
         });
     }, imgOptions);
+}   
 
-imagesToLoad.forEach((img)) => {
-    imgObserver.observe(img);
+    imagesToLoad.forEach((img) => {
+        imgObserver.observer(img)
     });
-}
+
 else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
-    });
+    })    
 }
+
+
+
