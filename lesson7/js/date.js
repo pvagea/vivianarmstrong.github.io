6c0fa03e3d092facfe2b0fbc1c37d7c97a181d5d
pvagea <arm18006@byui.edu>
2020-02-21 19:16:33 -0800

    g

--- lesson7/js/date.js
+++ lesson7/js/date.js
@@ -30,19 +30,3 @@ let currMonth = month[d.getMonth()];
 let currDay = weekday[d.getDay()];
 let stringDate = currDay + ", " + d.getDate() + " " + currMonth + " " + currentYear;
 document.getElementById("todayDate").textContent = stringDate;
-
-
-
-//Code for the banner message at the top of the webpage to show up only on Friday
-var today = new Date();
-var date = today.getDay();
-if (date == 5) {
-    let text = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    document.getElementById("message").innerHTML = text;
-    document.getElementById("message").style.backgroundColor = "#f9f960";
-    document.getElementById("message").style.fontFamily = "lato";
-    document.getElementById("message").style.textAlign = "center";
-    document.getElementById("message").style.padding = "0.80em";
-} else {
-
-}
