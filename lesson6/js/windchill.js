ef62a23e8b9a5978d545e5ac40c50376dce14008
pvagea <arm18006@byui.edu>
2020-02-24 20:16:56 -0800

    f

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,10 @@
 const temp = parseFloat(document.getElementById('temp').textContent);
 const ws = parseFloat(document.getElementById('ws').textContent);
 
-let wc = windChill(temp, s) 
-	if (temp <= 50 && s > 3) {
-		let windChill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp * Math.pow(ws, 0.16))).toFixed(1);
-		document.getElementById('windChill').textContent = windChill + "&#176;F";
+let wc = windChill(temp, ws) 
+	if (temp <= 50 && ws > 3) {
+		let windChill = (35.74 + (0.6215 * temp) - 35.75 * Math.pow(ws, 0.16) + 0.4275 * temp * Math.pow(ws, 0.16)).toFixed(1);
+		document.getElementById('windChill').textContent = windChill;
 	} else {
 		document.getElementById('windChill').textContent = "N/A";
 	}
