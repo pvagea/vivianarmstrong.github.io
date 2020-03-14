81337e324e8ef031efeef64da4b3d771a4130b6e
pvagea <arm18006@byui.edu>
2020-03-14 11:35:35 -0700

    k

--- lesson10/js/api-test.js
+++ lesson10/js/api-test.js
@@ -1,7 +1,13 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&unites=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+        const currentTemp = document.querySelector('#current-temp');
+        const weatherIcon = document.querySelector('#imagesrc');
+
+        currentTemp.textContent = jsObject.main.temp;
+
+
     })
