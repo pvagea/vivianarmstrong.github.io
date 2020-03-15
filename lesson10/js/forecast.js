5a5fe7b55d41bfe974dc2e51d3c67539b071b1e5
pvagea <arm18006@byui.edu>
2020-03-14 20:14:01 -0700

    k

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -3,23 +3,21 @@ const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-
+        
         const forecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
-            document.getElementById('currently').textContent = jsObject.weather[0].main;
-            document.getElementById('high').innerHTML = parseFloat(jsObject.main.temp).toFixed(0);
-            document.getElementById('humidity').innerHTML = jsObject.main.humidity;
-            document.getElementById('ws').textContent = jsObject.wind.speed;
+            
 
 
-        for (let i = 0; i < forecast.lenth; i++) {
+        for (let i = 0; i < forecast.length; i++) {
             document.getElementsById(`forecast${i+1}`).
             textContent = fivedayforecast[i].main.temp;
-        }
+        
+
 
         const imagesrc = `https://openweathermap.org/img/w${jsObject.weather[0].icon}.png`
         const desc = forescast[i].weather[0].description;
         document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
         document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
+    }
 
 })
--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,3 +1,16 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+		console.log(jsObject);
+        document.getElementById('temp').textContent = jsObject.weather[0].description;
+        document.getElementById('high').textContent = parseFloat(jsObject.main.temp_max).toFixed(0);
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('ws').textContent = jsObject.wind.speed.toFixed(0);
+});
+
+
 const temp = parseFloat(document.getElementById('temp').textContent);
 const ws = parseFloat(document.getElementById('ws').textContent);
 
