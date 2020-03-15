4b730287d28f825e25ba3ffe7f0fe8b27014502d
pvagea <arm18006@byui.edu>
2020-03-14 20:23:07 -0700

    k

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -3,17 +3,24 @@ const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
+		console.log(jsObject);
+        document.getElementById('temp').textContent = jsObject.weather[0].description;
+        document.getElementById('high').textContent = parseFloat(jsObject.main.temp_max).toFixed(0);
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('ws').textContent = jsObject.wind.speed.toFixed(0);
+});
+
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
         
         const forecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
-            
-
 
         for (let i = 0; i < forecast.length; i++) {
             document.getElementsById(`forecast${i+1}`).
             textContent = fivedayforecast[i].main.temp;
         
-
-
         const imagesrc = `https://openweathermap.org/img/w${jsObject.weather[0].icon}.png`
         const desc = forescast[i].weather[0].description;
         document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
