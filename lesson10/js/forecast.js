c501efb27320a1eb983fb1c15ef34813d769cb88
pvagea <arm18006@byui.edu>
2020-03-17 19:56:09 -0700

    l

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,9 +1,9 @@
 const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
 
 fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-		console.log(jsObject);
+    .then(function(response) { return response.json()}
+    .then((jsObject) {
+	//	console.log(jsObject);
         document.getElementById('temp').textContent = jsObject.weather[0].description;
         document.getElementById('high').textContent = parseFloat(jsObject.main.temp_max).toFixed(0);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
@@ -14,15 +14,15 @@ fetch(apiURL)
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        
+     //console.log(jsObject);   
         const forecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
 
         for (let i = 0; i < forecast.length; i++) {
             document.getElementsById(`forecast${i+1}`).
-            textContent = fivedayforecast[i].main.temp;
+            textContent = fivedayforecast[i].main.temp.toFixed(0);
         
         const imagesrc = `https://openweathermap.org/img/w${jsObject.weather[0].icon}.png`
-        const desc = forescast[i].weather[0].description;
+        const desc = forecast[i].weather[0].description;
         document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
         document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
     }
