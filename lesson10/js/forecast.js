bd5427a2aa25af3c7e89cc945f9984f972a7f7aa
pvagea <arm18006@byui.edu>
2020-03-21 16:48:54 -0700

    l

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -3,7 +3,7 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('temp').textContent = jsObject.weather[0].description;
+        document.getElementById('currently').textContent = jsObject.weather[0].description;
         document.getElementById('high').textContent = parseFloat(jsObject.main.temp_max).toFixed(0);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('ws').textContent = jsObject.wind.speed.toFixed(0);
