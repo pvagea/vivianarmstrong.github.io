8f9d3f61c89d2b5d592f65ffd5b1d12b9d260aad
pvagea <arm18006@byui.edu>
2020-03-21 18:19:58 -0700

    m

--- lesson11/js/weather.js
+++ lesson11/js/weather.js
@@ -25,20 +25,19 @@ let wc = windChill(temp, s)
 
 
 //Code for Forecast
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
+const apiURLB = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5155087f7a6b4b3be35a1e578260260f';
 
-fetch(apiURL)
+fetch(apiURLB)
     .then((response) => response.json())
     .then((jsObject) => {
-        
-        const forecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
-        for (let i = 0; i < forecast.length; i++) {
-            document.getElementsById(`forecast${i+1}`).
-            textContent = fivedayforecast[i].main.temp;
-        
-        const imagesrc = `https://openweathermap.org/img/w${jsObject.weather[0].icon}.png`
-        const desc = forescast[i].weather[0].description;
-        document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
+        const fivedayforecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
+        for (let i = 0; i < fivedayforecast.length; i++) {
+        document.getElementsById(`temp${i+1}`).textContent = fivedayforecast[i].main.temp;
+        const iconsrc = `https://openweathermap.org/img/w${iconName}.png`
+        const iconName = fivedayforecast[i].weather[0].icon;
+        const desc = fivedayforescast[i].weather[0].description;
+        document.getElementById(`icon${i+1}`).setAttribute('src', iconsrc);
         document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
     }
 })
+
