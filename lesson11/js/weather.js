a01707efd656e1e96a0d318584e4655b71927ee7
pvagea <arm18006@byui.edu>
2020-03-21 20:20:26 -0700

    m

--- lesson11/js/weather.js
+++ lesson11/js/weather.js
@@ -15,13 +15,13 @@ fetch(apiURL)
 const temp = parseFloat(document.getElementById('temp').textContent);
 const ws = parseFloat(document.getElementById('ws').textContent);
 
-let wc = windChill(temp, s) 
-	if (temp <= 50 && s > 3) {
-		let windChill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp * Math.pow(ws, 0.16))).toFixed(1);
-		document.getElementById('windChill').textContent = windChill + "&#176;F";
-	} else {
-		document.getElementById('windChill').textContent = "N/A";
-	}
+let wc = windChill(temp, s)
+if (temp <= 50 && s > 3) {
+    let windChill = (35.74 + (0.6215 * temp) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * temp * Math.pow(ws, 0.16))).toFixed(1);
+    document.getElementById('windChill').textContent = windChill + "&#176;F";
+} else {
+    document.getElementById('windChill').textContent = "N/A";
+}
 
 
 //Code for Forecast
@@ -30,14 +30,18 @@ const apiURLB = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&uni
 fetch(apiURLB)
     .then((response) => response.json())
     .then((jsObject) => {
-        const fivedayforecast = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
-        for (let i = 0; i < fivedayforecast.length; i++) {
-        document.getElementsById(`temp${i+1}`).textContent = fivedayforecast[i].main.temp;
-        const iconsrc = `https://openweathermap.org/img/w${iconName}.png`
-        const iconName = fivedayforecast[i].weather[0].icon;
-        const desc = fivedayforescast[i].weather[0].description;
-        document.getElementById(`icon${i+1}`).setAttribute('src', iconsrc);
-        document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
-    }
-})
+        const fivedays = jsObject.list.filter(x => x.dt_text.includes('18:00:00'));
+        for (let i = 0; i < fivedays.length; i++) {
 
+            var d = new Date(fivedays[i].dt_txt);
+            const weekday = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+            
+            document.getElementById(`day${i+1}`).textContent = weekday[document.getday()];
+            document.getElementsById(`temp${i+1}`).textContent = fivedays[i].main.temp;
+            
+            const imagesrc = 'https://openweathermap.org/img/w' + fivedays[i].weather[0].icon + '.png';
+            const desc = fivedays[i].weather[0].description;
+            document.getElementById(`icon${i+1}`).setAttribute('src', imagesrc);
+            document.getElementById(`icon${i+1}`).setAttribute('alt', desc);
+        }
+    })
